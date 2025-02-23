551c368fe58225e292fde4877563c83879c17053
Ksenia Voroshilova <ksenia.sevastopol5@gmail.com>
2025-02-23 11:31:48 +0300

    вывод объекта дерева, на который ссылается последний коммит

--- 20250217/1/prog.py
+++ 20250217/1/prog.py
@@ -14,6 +14,24 @@ def view_commit(commit_id):
     obj_text = obj.decode()
     print(obj_text)
 
+    _, obj_text_ = obj_text.split('tree ')
+    tree_id = obj_text_.split('\n')[0]
+    print('tree', tree_id)
+    view_tree(tree_id)
+
+def view_tree(tree_id):
+    tree_filepath = f'.git/objects/{tree_id[:2]}/{tree_id[2:]}'
+    with open(tree_filepath, 'rb') as tf:
+        obj = zlib.decompress(tf.read())
+    header, _, tail = obj.partition(b'\x00')
+    kind, _ = header.split()
+
+    if kind == b'tree':
+        while tail:
+            treeobj, _, tail = tail.partition(b'\x00')
+            _, tname = treeobj.split()
+            print(tname.decode())
+
 if len(sys.argv) == 1:
     for branch in iglob('.git/refs/heads/*'):
         print(basename(branch))
