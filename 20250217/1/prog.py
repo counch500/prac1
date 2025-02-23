0df613d9337c6c97f485ac9b02290fbd2dfdf32c
Ksenia Voroshilova <ksenia.sevastopol5@gmail.com>
2025-02-23 11:26:21 +0300

    вывод последнего коммита указанной ветки

--- 20250217/1/prog.py
+++ 20250217/1/prog.py
@@ -2,7 +2,15 @@ import sys
 from glob import iglob
 from os.path import basename
 
+def get_last_commit(branch):
+    branch_path = f'.git/refs/heads/{branch}'
+    with open(branch_path) as bp:
+        return bp.readline().strip()
+
 if len(sys.argv) == 1:
     for branch in iglob('.git/refs/heads/*'):
         print(basename(branch))
-
+else:
+    branch = sys.argv[1]
+    last_commit = get_last_commit(branch)
+    print("Последний коммит:", last_commit)
