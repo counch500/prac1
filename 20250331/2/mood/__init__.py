e1677b22f3ea8a5d835971218756f0fbe5e4df89
Ksenia Voroshilova <ksenia.sevastopol5@gmail.com>
2025-04-06 17:59:33 +0300

    done

---
+++ 20250331/2/mood/__init__.py
@@ -0,0 +1,8 @@
+"""MOOD (Multi-User Online Dungeon) - текстовая RPG игра на основе cowsay.
+
+Пакет содержит:
+- client - модуль клиента
+- server - модуль сервера 
+- common - общие компоненты
+"""
+
